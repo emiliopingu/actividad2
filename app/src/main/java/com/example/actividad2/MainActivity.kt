ff1aebaeb21509ee56903307aa1e167e6b814bd4
emilio <belmonteoilime@gmail.com>
2019-05-31 09:35:47 +0200

    final

--- app/src/main/java/com/example/actividad2/MainActivity.kt
+++ app/src/main/java/com/example/actividad2/MainActivity.kt
@@ -36,9 +36,6 @@ class MainActivity : AppCompatActivity() {
     private var selectedMonth = 0
     private var selectedDay = 0
 
-
-
-
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -95,25 +92,11 @@ class MainActivity : AppCompatActivity() {
                 if (!nombre.isEmpty() && !lugar.isEmpty() && !usuario.isEmpty()
                     && !descripcion.isEmpty() && !fecha.isEmpty()
                 ) {
-                    if (nombre.length in 31..3) {
-                        if (lugar.length in 31..0) {
-                            if (usuario.length in 21..0) {
-                                if (descripcion.length in 101..9) {
-                                    Repository(this).insertTask(nombre, lugar, usuario, descripcion, fecha.toString())
-                                    list.add(Task(0, nombre, lugar, usuario, descripcion, fecha.toString()))
-                                    Toast.makeText(this@MainActivity, "se ha guardado los datos", Toast.LENGTH_LONG).show()
-                                }else{
-                                    Toast.makeText(this,"La descripcion del problema debe de tener entre 100 y 10 caracteres",Toast.LENGTH_LONG).show()
-                                }
-                            }else{
-                                Toast.makeText(this,"El usuario debe de tener entre 30 y 4 caracteres",Toast.LENGTH_LONG).show()
-                            }
-                        }else{
-                            Toast.makeText(this,"El lugar del problema debe de tener entre 30 y 4 caracteres",Toast.LENGTH_LONG).show()
-                        }
-                    }else{
-                        Toast.makeText(this,"El nombre del problema debe de tener entre 30 y 4 caracteres",Toast.LENGTH_LONG).show()
-                    }
+
+                    Repository(this).insertTask(nombre, lugar, usuario, descripcion, fecha.toString())
+                    list.add(Task(0, nombre, lugar, usuario, descripcion, fecha.toString()))
+                    Toast.makeText(this@MainActivity, "se ha guardado los datos", Toast.LENGTH_LONG).show()
+
 
                 } else {
                     Toast.makeText(this@MainActivity, "Rellene los campos", Toast.LENGTH_LONG).show()
@@ -173,81 +156,81 @@ class MainActivity : AppCompatActivity() {
                 val descripcion = view.etDescripcionTareaEdit.text.toString()
 
                 if (!nombre.isEmpty() && !lugar.isEmpty() && !usuario.isEmpty()
-                    && !descripcion.isEmpty() && !fecha.isEmpty())
-                {
-                    if (nombre.length in 31..3) {
-                        if (lugar.length in 31..0) {
-                            if (usuario.length in 21..0) {
-                                if (descripcion.length in 101..9) {
-                                    for (x in 0 until list.size) {
-                                        if (list[x].name == nombre) {
-                                            list.removeAt(x) } }
-                                    Repository(this).updateTask(nombre, lugar, usuario, fecha, descripcion)
-                                    list.add(
-                                        Task(0, nombre, lugar, usuario, fecha, descripcion))
-                                }else{
-                                    Toast.makeText(this,"La descripcion del problema debe de tener entre 100 y 10 caracteres",Toast.LENGTH_LONG).show()
-                                }
-                            }else{
-                                Toast.makeText(this,"El usuario debe de tener entre 30 y 4 caracteres",Toast.LENGTH_LONG).show()
-                            }
-                        }else{
-                            Toast.makeText(this,"El lugar del problema debe de tener entre 30 y 4 caracteres",Toast.LENGTH_LONG).show()
+                    && !descripcion.isEmpty() && !fecha.isEmpty()
+                ) {
+
+                    for (x in 0 until list.size) {
+                        if (list[x].name == nombre) {
+                            list.removeAt(x)
                         }
-                    }else{
-                        Toast.makeText(this,"El nombre del problema debe de tener entre 30 y 4 caracteres",Toast.LENGTH_LONG).show()
                     }
+                    Repository(this).updateTask(nombre, lugar, usuario, fecha, descripcion)
+                    list.add(
+                        Task(0, nombre, lugar, usuario, fecha, descripcion)
+                    )
+                } else {
+                    Toast.makeText(
+                        this,
+                        "La descripcion del problema debe de tener entre 100 y 10 caracteres",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
 
-                showDialog.dismiss()
-                inflater()
-            }
-            view.cancelar.setOnClickListener {
-                showDialog.dismiss()
-            }
 
 
-        }
 
+        showDialog.dismiss()
+        inflater()
+    }
+    view.cancelar.setOnClickListener{
+        showDialog.dismiss()
     }
 
-    fun inflater() {
 
-        val layoutManager = LinearLayoutManager(this@MainActivity)
-        layoutManager.orientation = LinearLayoutManager.VERTICAL
-        recycleViewTareas.layoutManager = layoutManager
-        val adapter = AdapterTareas(this@MainActivity, list)
-        recycleViewTareas.adapter = adapter
-        val dividerItemDecoration = DividerItemDecoration(this , layoutManager.orientation)
-        recycleViewTareas.addItemDecoration(dividerItemDecoration)
+}
+
+}
+
+fun inflater() {
+
+    val layoutManager = LinearLayoutManager(this@MainActivity)
+    layoutManager.orientation = LinearLayoutManager.VERTICAL
+    recycleViewTareas.layoutManager = layoutManager
+    val adapter = AdapterTareas(this@MainActivity, list)
+    recycleViewTareas.adapter = adapter
+    val dividerItemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+    recycleViewTareas.addItemDecoration(dividerItemDecoration)
 
 
+}
 
+fun consult() {
+
+    val helper = TaskHelper(this)
+    val db = helper.writableDatabase
+    val cursor: Cursor = db.rawQuery(" SELECT * FROM " + ReadTask.Entry.TABLE_NAME, null)
+    while (cursor.moveToNext()) {
+
+        val name: String = cursor.getString(1)
+        val place: String = cursor.getString(2)
+        val user: String = cursor.getString(3)
+        val datee: String = cursor.getString(4)
+        val description: String = cursor.getString(5)
+        list.add(Task(0, name, place, user, datee, description))
 
     }
+}
 
-    fun consult() {
-
-        val helper = TaskHelper(this)
-        val db = helper.writableDatabase
-        val cursor: Cursor = db.rawQuery(" SELECT * FROM " + ReadTask.Entry.TABLE_NAME, null)
-        while (cursor.moveToNext()) {
-
-            val name: String = cursor.getString(1)
-            val place: String = cursor.getString(2)
-            val user: String = cursor.getString(3)
-            val datee: String = cursor.getString(4)
-            val description: String = cursor.getString(5)
-            list.add(Task(0, name, place, user, datee, description))
-
-        }
-    }
-
-    private fun setRecyclerViewItemTouchListener() {
+private fun setRecyclerViewItemTouchListener() {
 
 
-        val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
-            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, viewHolder1: RecyclerView.ViewHolder): Boolean {
+    val itemTouchCallback =
+        object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                viewHolder1: RecyclerView.ViewHolder
+            ): Boolean {
 
                 return false
             }
@@ -256,14 +239,20 @@ class MainActivity : AppCompatActivity() {
 
                 val position = viewHolder.adapterPosition
                 list.removeAt(position)
-                recycleViewTareas.adapter!!.notifyItemRemoved(position)
-             Repository(this@MainActivity).deleteTask(list[position].name)
 
+                if (position != 0) {
+                    recycleViewTareas.adapter!!.notifyItemRemoved(position)
+                    Repository(this@MainActivity).deleteTask(list[position - 1].name)
+
+                }else if(position==0){
+                    recycleViewTareas.adapter!!.notifyItemRemoved(position)
+                    Repository(this@MainActivity).deleteTask(list[0].name)
+                }
             }
         }
 
-        val itemTouchHelper = ItemTouchHelper(itemTouchCallback)
-        itemTouchHelper.attachToRecyclerView( recycleViewTareas)
-    }
+    val itemTouchHelper = ItemTouchHelper(itemTouchCallback)
+    itemTouchHelper.attachToRecyclerView(recycleViewTareas)
+}
 }
 
