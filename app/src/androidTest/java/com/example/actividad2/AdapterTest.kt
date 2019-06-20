8925e4efd81a8dee58f34dbf9fc6b33b9de45a2c
emilio <belmonteoilime@gmail.com>
2019-06-20 13:45:38 +0200

    adapterTesting

--- app/src/androidTest/java/com/example/actividad2/AdapterTest.kt
+++ app/src/androidTest/java/com/example/actividad2/AdapterTest.kt
@@ -1,24 +1,31 @@
 package com.example.actividad2
 
 import android.content.Intent
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.contrib.RecyclerViewActions
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers.assertThat
-import com.example.actividad2.data.Task
 import com.example.actividad2.presentation.DescripcionTareaActivity
 import com.example.actividad2.presentation.adapter.AdapterTareas
-
-
 import org.junit.Rule
 import org.junit.Test
 
+
 class AdapterTest {
-    @get:Rule
-    val intentsTestAdapterTareas = IntentsTestRule(AdapterTareas::class.java)
+   // @get:Rule
+   // val intentsTestAdapterTareas = IntentsTestRule(AdapterTareas::class.java)
     @Test
     fun intentfuntion(){
-        val i:Intent= Intent(intentsTestAdapterTareas,DescripcionTareaActivity::class.java)
-        intentsTestAdapterTareas.launchActivity(i)
-
+      //  val i:Intent= Intent(intentsTestAdapterTareas,DescripcionTareaActivity::class.java)
+       // intentsTestAdapterTareas.launchActivity(i)
     }
+
+    @Test
+    fun recycleView(){
+        onView(withId(R.id.recycleViewTareas))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<AdapterTareas.viewHolder>(0, click()))
+    }
+
 
 }
