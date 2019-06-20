197c52055d46a5ff1fe8f49a321b1f2e80e599ac
emilio <belmonteoilime@gmail.com>
2019-06-20 13:31:35 +0200

    mas testing

--- app/src/androidTest/java/com/example/actividad2/AdapterTest.kt
+++ app/src/androidTest/java/com/example/actividad2/AdapterTest.kt
@@ -3,7 +3,9 @@ package com.example.actividad2
 import android.content.Intent
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import com.example.actividad2.data.Task
 import com.example.actividad2.presentation.DescripcionTareaActivity
+import com.example.actividad2.presentation.adapter.AdapterTareas
 
 
 import org.junit.Rule
@@ -11,18 +13,11 @@ import org.junit.Test
 
 class AdapterTest {
     @get:Rule
-    val intentsTestRule = IntentsTestRule(DescripcionTareaActivity::class.java)
-
+    val intentsTestAdapterTareas = IntentsTestRule(AdapterTareas::class.java)
     @Test
     fun intentfuntion(){
-        assertThat(intent).hasAction(Intent.ACTION_VIEW)
-        assertThat(intent).categories().containsExactly(Intent.CATEGORY_BROWSABLE)
-        assertThat(intent).extras().containsKey("tareaDescripcion")
-        assertThat(intent).extras().containsKey("usuarioDescripcion")
-        assertThat(intent).extras().containsKey("problemaDescripcion")
-        assertThat(intent).extras().containsKey("lugarDescripcion")
-        assertThat(intent).extras().containsKey("fechaDescripcion")
-        
+        val i:Intent= Intent(intentsTestAdapterTareas,DescripcionTareaActivity::class.java)
+        intentsTestAdapterTareas.launchActivity(i)
 
     }
 
